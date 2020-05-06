aec5e95a8e41de7458906f37020a2278b10d7b34
ESSch <ESSchtolts@yandex.ru>
2020-05-06 21:00:07 +0000

    WebHook

---
+++ WebHook.sh
@@ -0,0 +1,4 @@
+URL=$(oc describe bc/python-app | grep -o 'http.*generic$')
+SECRET=$(oc get bc/python-app -o json | jq -r '.spec.triggers | .[] | select(.type == "Generic").generic.secret ')
+HOOCK=$(echo $URL | sed 's!< secret>!'$SECRET'!')
+curl -X POST -k $HOOCK
