d8139fd3c79d384b7f1444b1d23ccd9413409626
ESSch <ESSchtolts@yandex.ru>
2020-05-10 20:22:19 +0000

    gix

--- WebHook.sh
+++ WebHook.sh
@@ -1,4 +1,4 @@
 URL=$(oc describe bc/python-app | grep -o 'http.*generic$')
 SECRET=$(oc get bc/python-app -o json | jq -r '.spec.triggers | .[] | select(.type == "Generic").generic.secret ')
-export HOOCK=$(echo $URL | sed 's!< secret>!'$SECRET'!')
+export HOOCK=$(echo $URL | sed 's!<secret>!'$SECRET'!')
 # curl -X POST -k $HOOCK
