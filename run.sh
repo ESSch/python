c78cc3361c3eb1c84e1ad8a2a49e54d3d7bb40a7
ESSch <ESSchtolts@yandex.ru>
2020-05-11 16:32:54 +0000

    Template

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 oc new-app https://github.com/badri/python-app.git
-sleep 100
 git clone https://github.com/ESSch/python.git
 cd python/
 source WebHook.sh
 echo $HOOCK
-sed 's!\${HOOCK}!'$HOOCK'!' default.conf > default_hoock.conf
-oc -n openshift create configmap nginx --from-file=default_hoock.conf
-oc -n openshift create -f dc_nginx.yaml
-oc -n openshift create -f svc_nginx.yaml
-oc -n default create -f rout_nginx.yaml
+oc process -f tmt.yaml HOOCK=$HOOCK | oc get -f -
